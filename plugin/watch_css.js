3b6a969a56aabf4bb18a2dd0c0e89806438fb481
Adrian Stainforth <adrian@morsedigital.com>
2016-10-05 21:07:53 +0100

    fixing watcher for browsersync

--- plugin/watch_css.js
+++ plugin/watch_css.js
@@ -1,12 +1,12 @@
 var config = require('./config')
  , file_details = require('./get_file_details')
  , read = require('./read_css')
- , Build = require('./build_folder')
+ , Build = require('./build_folder');
 var Watch = require('@djforth/ap_utils').watcher;
 
 function processCSS(name, path, cb){
   if (name.match(/^_/)){
-    Build();
+    Build(cb);
   } else {
     var details = file_details(path, name);
     read(details, cb);
@@ -17,7 +17,7 @@ module.exports = function(cb){
   var watcher = Watch(config.get('input'));
   watcher.onAdd(function(name, path){
     if (!name.match(/^_/)){
-      Build();
+      Build(cb);
     }
   })
   .addChange(function(name, path){
