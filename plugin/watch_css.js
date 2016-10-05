3c6573886d429f16078b55dbd5cdeab1b4cc2421
Adrian Stainforth <adrian@morsedigital.com>
2016-10-05 14:58:40 +0100

    fix watch

--- plugin/watch_css.js
+++ plugin/watch_css.js
@@ -1,18 +1,24 @@
-
-var config      = require('../plugin/config')
- , file_details = require('../plugin/get_file_details')
- , read         = require('../plugin/read_css');
-var Watch        = require('@djforth/ap_utils').watcher;
+var config = require('./config')
+ , file_details = require('./get_file_details')
+ , read = require('./read_css')
+ , Build = require('./build_folder')
+var Watch = require('@djforth/ap_utils').watcher;
 
 function processCSS(name, path, cb){
-  var details = file_details(path, name);
-  read(details, cb);
+  if (name.match(/^_/)){
+    Build();
+  } else {
+    var details = file_details(path, name);
+    read(details, cb);
+  }
 }
 
 module.exports = function(cb){
   var watcher = Watch(config.get('input'));
   watcher.onAdd(function(name, path){
-    processCSS(name, path, cb);
+    if (!name.match(/^_/)){
+      Build();
+    }
   })
   .addChange(function(name, path){
     processCSS(name, path, cb);
